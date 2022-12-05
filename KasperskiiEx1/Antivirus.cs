d37dd427d2a6709c16b855c4f25111cb810a5507
Сергей <skirflex1488@gmail.com>
2022-12-05 13:05:03 +0300

    Добавьте файлы проекта.

---
+++ KasperskiiEx1/Antivirus.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Diagnostics;
+
+namespace KasperskiiEx1
+{
+    static class Antivirus
+    {
+        static int jsDetects;
+        static int rmDetects;
+        static int Rundll32;
+        static int countErorrs;
+        static int processedFiles;
+        static double time;
+
+        public static void Start(string [] args)
+        {
+            var findTime = new Stopwatch();
+            findTime.Start();
+            jsDetects = 0;
+            rmDetects = 0;
+            Rundll32 = 0;
+            countErorrs = 0;
+            
+             
+            var optionsForDirectory = new EnumerationOptions();
+
+            string path = args.Length > 0 ? args[0] : @"C:\Users\user\Desktop\Test";
+            var files = Directory.GetFiles(path);
+            processedFiles = files.Length;
+            foreach (string file in files)
+            {
+                bool isDetected = false;
+                try
+                {
+                    using (var reader = new StreamReader(file))
+                    {
+                        while (!reader.EndOfStream || isDetected)
+                        {
+                            string str = reader.ReadLine();
+                            
+                            if (file.Substring(file.LastIndexOf('.')) == ".js" && str.Equals("<script>evil_script()</script>"))
+                            {
+                                jsDetects++;
+                                isDetected = false;
+
+                            }
+                            else if (str.Equals("rm -rf %userprofile%\\Documents"))
+                            {
+                                rmDetects++;
+                                isDetected = false;
+                            }
+                            else if (str.Equals("Rundll32 sus.dll SusEntry"))
+                            {
+                                Rundll32++;
+                                isDetected = false;
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    countErorrs++;
+                    
+                }
+                
+            }
+            findTime.Stop();
+            time = findTime.Elapsed.Seconds;
+            GetInfo();
+        }
+
+        private static void GetInfo()
+        {
+            Console.WriteLine($@"===== Scan result =====
+Processed files: {processedFiles}
+JS detects: {jsDetects}
+rm -rf detects: {rmDetects}
+Rundll32 detects: {Rundll32}
+Errors: {countErorrs}
+Exection time: {time:00:00:00}
+=======================");
+        }
+    }
+}
---
+++ KasperskiiEx1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace KasperskiiEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Antivirus.Start(args);
+        }
+    }
+}
